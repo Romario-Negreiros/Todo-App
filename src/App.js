a2a86977d5898044482e80720acdcdeb4f1d45aa
Romario-Negreiros <nromario482@gmail.com>
2021-06-21 15:07:46 -0300

    bug when deleting a completed task fixed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Manager from './Manager.js'
 
 function App() {
   const [toDo, setToDo] = useState('')
+  const [states, setState] = useState('')
   // Function that receives the task from the input component
   // And then refresh the tasks
   const sendTask = task => {
@@ -13,43 +14,54 @@ function App() {
     setToDo(getAllTasks)
   }
   // Function to delete a task
-  const deleteTask = index => {
+  const deleteTask = (index, task) => {
     const getAllTasks = [...toDo]
     getAllTasks.splice(index, 1)
-    setToDo(getAllTasks)  
+    setToDo(getAllTasks)
+    if (states.includes(task)) {
+      const newStates = states.filter(v => v !== task)
+      setState(newStates)
+    }
+  }
+  const taskHasBeenDone = task => {
+    const getAllTasks = [...toDo]
+    const taskDone = getAllTasks.find(v => v == task)
+    const updateStates = [...states]
+    if (updateStates.includes(taskDone)) {
+      const removeState = updateStates.filter(v => v !== taskDone)
+      setState(removeState)
+    } else {
+      updateStates.push(taskDone)
+      setState(updateStates)
+    }
   }
   // Creating the task components, and passing the props
   const getAllTasks = [...toDo]
-  const tasksList = getAllTasks.map((v, i) => <Task 
-    key={i} index={i} task={v} deleteTask={deleteTask} />)
-  // Management functions
-  const seeAllTasks = () => {
-    console.log('fungada')
-  }
-  const seeActiveTasks = () => {
-    console.log('funfed')
-  }
-  const seeCompletedTasks = () => {
-    console.log('funfadenoveo')
-  }
-  const clearCompletedTasks = () => {
-    console.log('funfa')
-  }
+  const tasksList = getAllTasks.map((v, i) => {
+    if (states.includes(v)) {
+      return <Task
+        key={i} index={i} task={v}
+        deleteTask={deleteTask}
+        taskHasBeenDone={taskHasBeenDone}
+        completed={true} />
+    } else {
+      return <Task
+        key={i} index={i} task={v}
+        deleteTask={deleteTask}
+        taskHasBeenDone={taskHasBeenDone}
+        completed={false} />
+    }
+  })
   return (
     <section>
       <InputTask sendTask={sendTask} />
 
       <ul className="c-tasksList">
+
         {tasksList}
-        <Manager 
-            length={toDo.length} 
-            seeAllTasks={seeAllTasks}
-            seeActiveTasks={seeActiveTasks}
-            seeCompletedTasks={seeCompletedTasks}
-            clearCompletedTasks={clearCompletedTasks} />
       </ul>
     </section>
   )
 }
 
-export default App;
+export default App
--- src/Task.js
+++ src/Task.js
@@ -4,24 +4,13 @@ import Delete from './images/icon-cross.svg'
 import Check from './images/icon-check.svg'
 
 function Task(props) {
-    const taskHasBeenDone = () => {
-        const paragraph = document.querySelector(`.task${props.index} div p`)
-        const circle = paragraph.previousSibling
-        const check = circle.firstChild
-        if(paragraph.innerHTML.includes('del')) {
-            paragraph.innerHTML = props.task
-            circle.classList.remove('completed')
-            check.src = ''
-        } else {
-            paragraph.innerHTML = `<del>${props.task}</del>`
-            circle.classList.add('completed')
-            check.src = Check
-        }
-    }
+    if(!props.completed) {
     return (
         <li className={`task${props.index} tasks`}>
             <div>
-                <div className="circle" onClick={taskHasBeenDone}>
+                <div 
+                    className="circle" 
+                    onClick={() => props.taskHasBeenDone(props.task)}>
                     <img src="" alt="" />
                 </div>
                 <p>{props.task}</p>
@@ -30,10 +19,30 @@ function Task(props) {
                 <img className="delete"
                     src={Delete}
                     alt="delete"
-                    onClick={() => props.deleteTask(props.index)} />
+                    onClick={() => props.deleteTask(props.index, props.task)} />
             </div>
         </li>
     )
+    } else {
+        return (
+            <li className={`task${props.index} tasks`}>
+                <div>
+                    <div 
+                        className="circle completed" 
+                        onClick={() => props.taskHasBeenDone(props.task)}>
+                        <img src={Check} alt="checked" />
+                    </div>
+                    <p><del>{props.task}</del></p>
+                </div>
+                <div>
+                    <img className="delete"
+                        src={Delete}
+                        alt="delete"
+                        onClick={() => props.deleteTask(props.index, props.task)} />
+                </div>
+            </li>
+        )   
+    }
 }
 
 export default Task
