7b52bf1d4484f367d144d37065eff8fbf83ea0ea
Romario-Negreiros <nromario482@gmail.com>
2021-06-25 23:44:36 -0300

    import fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './Header.js';
 import InputTask from './InputTask.js'
 import Task from './Task.js'
 import Manager from './Manager.js'
-import backToTop from './images/arrow-up.svg'
 
 
 function App() {
